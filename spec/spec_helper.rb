1b9c80da6a29ce8dc151678bef3732c6cdb93da0
Ahmy Yulrizka <yulrizka@gmail.com>
2013-08-14 21:39:09 +0200

    clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,12 @@
 require 'bundler/setup'
 require 'rspec'
 require 'cs'
-#require 'vcr'
 require 'pry'
 
 Dir[File.join(File.dirname(__FILE__),("support/**/*.rb"))].each {|f| require f}
 
-#CONFIG = YAML.load(File.read(File.expand_path("support/spec_config.yml", File.dirname(__FILE__))))
-
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
-
 end
 
 def create_client
