9a26c4a7f6c1219626ce3f207ac6f44d6a55e2a1
Ahmy Yulrizka <yulrizka@gmail.com>
2013-08-18 10:41:06 +0200

    refactor parameter_processor to it's own module

---
+++ lib/cs/parameter_processor.rb
@@ -0,0 +1,97 @@
+module CS
+  module ParameterProcessor
+    def process_alias!(params)
+      return if self.class.parameters_alias.nil?
+
+      aliases = params.select { |param| self.class.parameters_alias.include?(param) }
+      aliases.each do |alias_to, value|
+        alias_for = self.class.parameters_alias[alias_to]
+        params[alias_for] = value
+        params.delete(alias_to)
+      end
+    end
+
+    def process_valid_values(name, value, param_option)
+      return value unless param_option[:valid_values]
+
+      if param_option[:valid_values].include?(value)
+        value
+      elsif !value.nil?
+        raise ArgumentError, "Invalid value for parameter '#{name}'"
+      end
+    end
+
+    def process_default_value(name, value, param_option)
+      value.nil? ? param_option[:default] : value
+    end
+
+
+    def process_param(name, value, param_option)
+      retval = value
+      retval = process_param_integer(name, value, param_option) if param_option[:type] == Integer
+      retval = process_param_boolean(name, value, param_option) if param_option[:type] == Boolean
+      retval = process_param_string(name, value, param_option) if param_option[:type] == String
+      retval = process_param_time(name, value, param_option) if param_option[:type] == Time
+      retval
+    end
+
+    def process_param_integer(name, value, param_option)
+      if value.kind_of?(Integer)
+        retval = value
+        retval = process_valid_values(name, value, param_option) if param_option[:valid_values]
+        maximum = param_option[:maximum]
+        retval = maximum if maximum && retval > maximum
+      end
+
+      retval = process_default_value(name, retval, param_option)
+
+      if !value.nil?  && !value.kind_of?(Integer)
+        raise ArgumentError, "Received non Integer value for parameter '#{name}'"
+      end
+
+      retval
+    end
+
+    def process_param_boolean(name, value, param_option)
+      retval = nil
+
+      if value.nil?
+        retval = param_option[:default] ? param_option[:default] : nil
+      elsif [0, "0", false, "false"].include?(value)
+        retval = 0
+      else
+        retval = 1
+      end
+
+      retval
+    end
+
+    def process_param_string(name, value, param_option)
+      retval = value
+      retval = process_valid_values(name, value, param_option) if param_option[:valid_values]
+      retval = process_default_value(name, retval, param_option)
+
+      retval
+    end
+
+    # This method prosess assignment for properties with datatype Time.
+    # There are 3 type that is valid for this properties:
+    #
+    # * **Time**
+    # * **Numeric**. It will convert this to ruby {Time}
+    # * **Obejct that respond to #to_time** and return ruby {Time}
+    #
+    def process_param_time(name, value, param_option)
+      retval = value
+
+      if !value.nil?
+        retval =  CS::Time.new(value)
+      end
+
+      retval = process_default_value(name, retval, param_option)
+      retval = process_valid_values(name, value, param_option) if param_option[:valid_values]
+
+      retval
+    end
+  end
+end
--- lib/cs/relation.rb
+++ lib/cs/relation.rb
@@ -1,6 +1,6 @@
 module CS
+  module Boolean; end
   module Relation
-    module Boolean; end
 
     def check_session!
       raise Error::SessionEmptyError unless @session
@@ -27,27 +27,6 @@ module CS
 
     def parameters
       self.class.parameters
-    end
-
-    def get_options(input={})
-      options = {}
-
-      self.class.parameters.each do |parameter, param_option|
-        value = self.parameter(parameter)
-        value = input[parameter] if input.has_key?(parameter)
-
-        value = process_param_integer(parameter, value, param_option) if param_option[:type] == Integer
-        value = process_param_boolean(parameter, value, param_option) if param_option[:type] == Boolean
-        value = process_param_string(parameter, value, param_option) if param_option[:type] == String
-
-        if value.kind_of?(Time)
-          value = value.to_f
-        end
-
-        options[parameter] = value if value
-      end
-
-      options
     end
 
     def get_data(params={})
@@ -106,28 +85,37 @@ module CS
     end
 
     def inspect
-      #inspection = self.class.parameters.collect {|k,v| "#{k}: #{parameter(k).inspect}"}.compact.join(", ")
-      #"#<#{self.class} #{inspection}>"
-      #anonymous
       entries = to_a.take(11).map!(&:inspect)
       entries[10] = '...' if entries.size == 11
 
       "#<#{self.class.name} [#{entries.join(", \n")}]>"
     end
 
+    def get_options(input={})
+      options = {}
+
+      self.class.parameters.each do |name, param_option|
+        value = self.parameter(name) # get value from object
+        value = input[name] if input.has_key?(name) # override
+
+        value = process_param(name, value, param_option)
+        value = value.to_f if value.kind_of?(Time)
+
+        options[name] = value if value
+      end
+
+      options
+    end
+
     def where(params={})
       process_alias!(params)
       params.keep_if {|k| self.class.parameters[k]}
 
-      params.each do |k,v|
-        param_option = self.class.parameters[k]
+      params.each do |name,value|
+        param_option = self.class.parameters[name]
 
-        value = process_param_integer(k, v, param_option) if param_option[:type] == Integer
-        value = process_param_boolean(k, v, param_option) if param_option[:type] == Boolean
-        value = process_param_string(k, v, param_option) if param_option[:type] == String
-        value = process_param_time(k, v, param_option) if param_option[:type] == Time
-
-        self.send("#{k}=", value)
+        value = process_param(name, value, param_option)
+        self.send("#{name}=", value)
       end
 
       self
@@ -180,6 +168,7 @@ module CS
     protected
     def self.included(base)
       base.send(:include, Enumerable)
+      base.send(:include, ParameterProcessor)
       base.extend(ClassMethod)
       base.class_eval do
         attr_accessor :session
@@ -196,92 +185,6 @@ module CS
 
     def get_single_resource(params={})
       raise Error::NotImplementedError, "get_single_resource is not implemented for class : #{self.class}"
-    end
-
-    def process_alias!(params)
-      return if self.class.parameters_alias.nil?
-
-      aliases = params.select { |param| self.class.parameters_alias.include?(param) }
-      aliases.each do |alias_to, value|
-        alias_for = self.class.parameters_alias[alias_to]
-        params[alias_for] = value
-        params.delete(alias_to)
-      end
-    end
-
-
-    def process_valid_values(name, value, param_option)
-      return value unless param_option[:valid_values]
-
-      if param_option[:valid_values].include?(value)
-        value
-      elsif !value.nil?
-        raise ArgumentError, "Invalid value for parameter '#{name}'"
-      end
-
-    end
-
-    def process_default_value(name, value, param_option)
-      value.nil? ? param_option[:default] : value
-    end
-
-    def process_param_integer(name, value, param_option)
-      if value.kind_of?(Integer)
-        retval = value
-        retval = process_valid_values(name, value, param_option) if param_option[:valid_values]
-        maximum = param_option[:maximum]
-        retval = maximum if maximum && retval > maximum
-      end
-
-      retval = process_default_value(name, retval, param_option)
-
-      if !value.nil?  && !value.kind_of?(Integer)
-        raise ArgumentError, "Received non Integer value for parameter '#{name}'"
-      end
-
-      retval
-    end
-
-    def process_param_boolean(name, value, param_option)
-      retval = nil
-
-      if value.nil?
-        retval = param_option[:default] ? param_option[:default] : nil
-      elsif [0, "0", false, "false"].include?(value)
-        retval = 0
-      else
-        retval = 1
-      end
-
-      retval
-    end
-
-    def process_param_string(name, value, param_option)
-      retval = value
-      retval = process_valid_values(name, value, param_option) if param_option[:valid_values]
-      retval = process_default_value(name, retval, param_option)
-
-      retval
-    end
-
-    # This method prosess assignment for properties with datatype Time.
-    # There are 3 type that is valid for this properties:
-    #
-    # * **Time**
-    # * **Numeric**. It will convert this to ruby {Time}
-    # * **Obejct that respond to #to_time** and return ruby {Time}
-    #
-    def process_param_time(name, value, param_option)
-      retval = value
-
-      if !value.nil?
-        retval =  CS::Time.new(value)
-      end
-
-      retval = process_default_value(name, retval, param_option)
-      retval = process_valid_values(name, value, param_option) if param_option[:valid_values]
-
-      retval
     end
   end
 end
