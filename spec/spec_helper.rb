21150ae5cec0a3cc78eb9c5c8599bb4ececb2f46
Ahmy Yulrizka <yulrizka@gmail.com>
2014-11-16 23:03:10 +0100

    Fix code climate test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,24 @@
+#ENV['CODECLIMATE_REPO_TOKEN'] = "a4b19928b743eda49f7acaba02190126122bfa09bb2615395aa1820a7cce9fc0"
+# code climate
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require 'bundler/setup'
 require 'rspec'
 require 'cs'
 require 'pry'
-require 'webmock/rspec'
 
-# code climate
-WebMock.disable_net_connect!(:allow => "codeclimate.com")
-require "codeclimate-test-reporter"
-ENV['CODECLIMATE_REPO_TOKEN'] = "a4b19928b743eda49f7acaba02190126122bfa09bb2615395aa1820a7cce9fc0"
-CodeClimate::TestReporter.start
 
 Dir[File.join(File.dirname(__FILE__),("support/**/*.rb"))].each {|f| require f}
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.after(:suite) do
+      WebMock.disable_net_connect!(:allow => 'codeclimate.com')
+  end
 end
 
 def create_client
